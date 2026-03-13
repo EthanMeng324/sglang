4c1c4e7bf17e4648b63fccb40dd594a3d7ced2fe
EthanMeng324 <jackmenghan@gmail.com>
2026-03-13 05:06:13 +0000

    script

--- run_profile.sh
+++ run_profile.sh
@@ -1,3 +1,8 @@
-bash offload_profiling/run.sh
-bash offload_profiling/run_old.sh
-bash offload_profiling/analyze_nsys.sh
+bash offload_profiling/run_access.sh
+bash offload_profiling/run_access_old.sh
+bash offload_profiling/run_access_no.sh
+module purge
+module load Miniforge3/25.3.0-3
+module load CUDA/12.8.0
+module load GCC/12.3.0
+bash offload_profiling/analyze_nsys.sh real
